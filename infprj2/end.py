0f8f7e8e3dabd2695d1d1d715db6e27c8710bef6
stvanderhelm <0929380@hr.nl>
2017-01-18 16:16:59 +0100

    Comments toegevoegd end screen

--- infprj2/end.py
+++ infprj2/end.py
@@ -5,9 +5,14 @@ def update(game):
     pass
 
 def draw(game):
+    # Eindscherm kleur
     game.screen.fill((199, 251, 126))
     font = pygame.font.Font(None, 72)
-    label_1 = font.render("Gewonnen!! (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧", 1, (255,8,148))
+
+    # Eindscherm tekst + kleur
+    label_1 = font.render("Gewonnen!!", 1, (255,8,148))
     size = font.size("Gewonnen!!")
+
+    # Plaatsing Tekst in scherm
     game.screen.blit(label_1,(int(game.width/2 - (size[0]/2)), game.height/2 - (size[1]/2)))
     
