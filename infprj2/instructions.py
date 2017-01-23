aacad93a2bd49f75c257b01659bf20dad3554e77
eileenlopes <0933995@hr.nl>
2017-01-23 12:24:46 +0100

    Added regels to instructions

--- infprj2/instructions.py
+++ infprj2/instructions.py
@@ -9,11 +9,9 @@ def update(game):
     pass
 
 def init(game):
-	textbox.create(game,32,60,game.width * 0.5, "Hi", lambda x: game.set_state(7))
-
+	pass
 
 def draw(game):
-	file = open("assets\instructions.txt", "r")
-	data = file.read()
-	textbox.draw(game)
+	bg = pygame.image.load("assets/regels.jpg")
+	game.screen.blit(bg,(0,0))
 	button.draw(game, 10, 10, game.width / 10, game.height / 20, translate.translate("BACK"), 20, (25,25,25), (255,255,255), lambda x: game.set_state(game.last_state))
