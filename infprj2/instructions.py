1015e01c45c31e178d6cbe0b3cf063ecaf8d4852
Thijs <thyss22@gmail.com>
2017-01-23 12:52:10 +0100

    Improved instructions screen

--- infprj2/instructions.py
+++ infprj2/instructions.py
@@ -12,6 +12,6 @@ def init(game):
 	pass
 
 def draw(game):
-	bg = pygame.image.load("assets/regels.jpg")
-	game.screen.blit(bg,(0,0))
-	button.draw(game, 10, 10, game.width / 10, game.height / 20, translate.translate("BACK"), 20, (25,25,25), (255,255,255), lambda x: game.set_state(game.last_state))
+    bg = pygame.image.load("assets/regels.png")
+    game.screen.blit(bg,(0,0))
+    button.draw(game, 10, 10, game.width / 10, game.height / 20, translate.translate("BACK"), 20, (25,25,25), (255,255,255), lambda x: game.set_state(game.last_state))
