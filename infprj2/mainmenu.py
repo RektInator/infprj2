752fab331ca2795a4750d3ee8b307fe6f1f8a0d8
Thijs <thyss22@gmail.com>
2017-01-23 10:52:52 +0100

    Made instructions button translate

--- infprj2/mainmenu.py
+++ infprj2/mainmenu.py
@@ -25,7 +25,7 @@ def draw(game):
     button.draw(game, btn_x_off, btn_y_off(1), int(btn_width), int(btn_height), "Start", 20,      (25,25,25), (255,255,255), lambda x: game.set_state(2))
     button.draw(game, btn_x_off, btn_y_off(2), int(btn_width), int(btn_height), "Multiplayer", 20,      (25,25,25), (255,255,255), lambda x: game.set_state(6))    
     button.draw(game, btn_x_off, btn_y_off(3), int(btn_width), int(btn_height), translate.translate("OPTIONS"), 20,     (25,25,25), (255,255,255), lambda x: game.set_state(1))
-    button.draw(game, btn_x_off, btn_y_off(4), int(btn_width), int(btn_height), "Instructies", 20,      (25,25,25), (255,255,255), lambda x: game.set_state(7))
+    button.draw(game, btn_x_off, btn_y_off(4), int(btn_width), int(btn_height), translate.translate("INSTRUCTIONS"), 20,      (25,25,25), (255,255,255), lambda x: game.set_state(7))
     button.draw(game, btn_x_off, btn_y_off(5), int(btn_width), int(btn_height), "Leaderboard", 20,      (25,25,25), (255,255,255), lambda x: game.set_state(5))
     button.draw(game, btn_x_off, btn_y_off(6), int(btn_width), int(btn_height), translate.translate("QUIT"), 20,      (25,25,25), (255,255,255), lambda x: game.exit())
     # button.draw(game, btn_x_off, btn_y_off(7), int(btn_width), int(btn_height), "Afsluiten", 20,  (25,25,25), (255,255,255), game.exit())
