de066b42a3bcdcfbebdd7c8fb8cd4d0e3d190d8a
eileenlopes <0933995@hr.nl>
2017-01-23 10:42:38 +0100

    Added instructions button on main menu

---
+++ infprj2/instructions.py
@@ -0,0 +1,15 @@
+# Options menu python file
+import pygame
+import button
+import config
+import textbox
+import translate
+
+def update(game):
+    pass
+
+def init(game):
+	pass
+
+def draw(game):
+    pass
