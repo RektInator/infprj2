36d9c38e6ff2f056b9ee033adf5e2cbee90a07fc
Thijs <thyss22@gmail.com>
2017-01-25 21:43:40 +0100

    textbox in lobby

--- infprj2/lobby.py
+++ infprj2/lobby.py
@@ -1,10 +1,19 @@
 import pygame
+import textbox
 
 def draw(game):
+    if game.name == "":
+        textbox.draw(game)
     pass
+
+def ExecuteCommand(game,box,isEnterPressed):
+    if isEnterPressed:
+        game.name = box.text
 
 def update(game):
     pass
 
 def init(game):
+    if game.name == "":
+        textbox.create(game, 32, 32, 300, "", lambda game,box,isEnterPressed: ExecuteCommand(game,box,isEnterPressed))
     pass
