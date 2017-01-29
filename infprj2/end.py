ea05d3b67aa3a2896e48d634292aa5a2bdb4d45d
Thijs <thyss22@gmail.com>
2017-01-29 19:45:48 +0100

    restarting game now works

--- infprj2/end.py
+++ infprj2/end.py
@@ -13,6 +13,13 @@ def draw(game):
     # Termination screen colour
     game.screen.fill((160, 187, 194))
     font = pygame.font.Font(None, 72)
+    if game.has_started == True:
+        game.players = []
+        game.playercount = 0
+        game.current_player = 0
+        game.has_started = False
+        game.question = 0
+        game.chosen = []
 
     # Termination screen text + colour
     label_1 = font.render(game.winner + " " + translate.translate("PLAYER_WON"), 1, (212,175,55))
